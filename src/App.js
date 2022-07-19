3870868f16ec46d960288dd3c18234c39f9e2a1b
shltv <miras.sh03@gmail.com>
2022-07-19 09:45:11 +0600

    saving in case I fail to add maps

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
   const [userInput, setUserInput] = useState('')
   const [infoAboutCity, setInfoAboutCity] = useState({ localtime: '', temp_c: '', desc: '', uv: '' })
   const [result, setResult] = useState(false)
+  const [showResult, setShowResult] = useState(false)
+  const [askedHint, setAskedHint] = useState(false)
 
   const handleInputChange = (event) => {
     setUserInput(event.target.value)
@@ -18,15 +20,21 @@ function App() {
   function getCity() {
     setRandomCity(cities[Math.floor(Math.random() * cities.length)])
     setResult(false)
+    setShowResult(false)
+    setAskedHint(false)
   }
 
   function handleCheck() {
-    setResult(userInput.toLowerCase() === coords[3].toLowerCase())
-    setUserInput('')
+    if (userInput.toLowerCase() === coords[3].toLowerCase()) {
+      setResult(true)
+      setUserInput('')
+    }
+    setShowResult(true)
   }
 
   function handleGetHints() {
     if (coords[3]) {
+      setAskedHint(true)
       var config = {
         method: 'post',
         url: `https://api.weatherapi.com/v1/current.json?key=774610bc93ad427ab0d54259220807&q=${coords[1]},${coords[0]}`,
@@ -54,12 +62,11 @@ function App() {
 
       axios(config)
         .then(function (response) {
-          console.log(response)
           setCoords([response.data.longt, response.data.latt, response.data.standard.countryname, response.data.standard.city])
           setInfoAboutCity({ localtime: '', temp_c: '', desc: '', uv: '' })
         })
         .catch(function (error) {
-          console.warn(error.response.data.error.message)
+          console.error(error.response.data.error.message)
         });
     }
   }, [randomCity]);
@@ -69,28 +76,30 @@ function App() {
       <div className='app'>
         <div className={coords.length === 0 ? 'main-part-wrapper disable' : 'main-part-wrapper'}>
           <div id='desc-wrapper'>
-            <h3 class='text'>{coords[2]}</h3>
-            <h4 class='text'>{coords[3]}</h4>
-            <p class='text'>long = {coords[0]}</p>
-            <p class='text'>latt = {coords[1]}</p>
-            <p class='text'>localtime = {infoAboutCity.localtime}</p>
-            <p class='text'>temp_c = {infoAboutCity.temp_c}</p>
-            <p class='text'>desc = {infoAboutCity.desc}</p>
-            <p class='text'>uv = {infoAboutCity.uv}</p>
+            <h3 className='text'>{coords[2]}</h3>
+            <h4 className='text'>{coords[3]}</h4>
+            <p className='text'>Longitude = <b> {coords[0]} </b></p>
+            <p className='text'>Latitude = <b> {coords[1]} </b></p>
+            <div className={askedHint ? 'enable' : 'disable'}>
+              <p className='text'>Local time = <b> {infoAboutCity.localtime} </b></p>
+              <p className='text'>Temperature = <b> {infoAboutCity.temp_c}°C </b></p>
+              <p className='text'>Weather = <b> {infoAboutCity.desc} </b></p>
+              <p className='text'>UV = <b> {infoAboutCity.uv} </b></p>
+            </div>
           </div>
 
           <div className='give-hint-wrapper'>
-            <button onClick={handleGetHints} type="submit">Give me a hint</button>
+            <button id='hint-button' onClick={handleGetHints} type="submit">Give me a hint</button>
           </div>
         </div>
 
         <div id='int-area'>
           <button onClick={getCity} type="submit">Get city</button>
-          <input onChange={handleInputChange} value={userInput}></input>
+          <input onChange={handleInputChange} value={userInput} type='text'></input>
           <button onClick={handleCheck} type="submit">Check my guess</button>
         </div>
 
-        <h3 className={result ? 'enable' : 'disable'}>you are right</h3>
+        <h3 className={showResult ? 'enable' : 'disable'}>you are {result ? 'right' : 'wrong'}</h3>
       </div >
       <div className='map'></div>
     </>
