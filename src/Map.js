2a154c7c1d838d4a2bcf0d3077071555cbfc3f2b
shltv <miras.sh03@gmail.com>
2022-07-20 11:13:41 +0600

    minor changes

--- src/Map.js
+++ src/Map.js
@@ -1,13 +1,13 @@
 import { GoogleMap, Marker } from '@react-google-maps/api';
 import { Wrapper } from '@googlemaps/react-wrapper';
-import { useState } from 'react';
+// import { useState } from 'react';
 
-export const Map = () => {
+export const Map = ({ position, setPosition }) => {
     // console.log(text)
     // const Map = ({ position }) => {
 
     const MapsKey = process.env.REACT_APP_MAPS_API
-    const [position, setPosition] = useState({ lat: 43.238949, lng: 76.889709 })
+    // const [position, setPosition] = useState({ lat: 43.238949, lng: 76.889709 })
 
     const handleMapClick = (event) => {
         console.log(event.latLng)
@@ -23,6 +23,7 @@ export const Map = () => {
         fullscreenControl: false,
         mapTypeControl: false,
         streetViewControl: false,
+        zoomControl: true,
         minZoom: 2,
         maxZoom: 18
     };
@@ -32,12 +33,13 @@ export const Map = () => {
             <Wrapper apiKey={MapsKey}>
                 <GoogleMap
                     mapContainerStyle={containerStyle}
-                    center={position}
+                    center={undefined}
                     zoom={0}
                     clickableIcons={false}
                     draggable={true}
                     options={defaultMapOptions}
                     onClick={handleMapClick}
+                    mapTypeId={'terrain'}
                 >
 
 
