d19fa59ebf7e8cdf0125ab1af67b560f39671299
shltv <miras.sh03@gmail.com>
2022-07-20 10:17:42 +0600

    marker changes on press

--- src/Map.js
+++ src/Map.js
@@ -1,10 +1,18 @@
 import { GoogleMap, Marker } from '@react-google-maps/api';
 import { Wrapper } from '@googlemaps/react-wrapper';
+import { useState } from 'react';
 
-export const Map = ({ position }) => {
+export const Map = () => {
     // console.log(text)
     // const Map = ({ position }) => {
+
     const MapsKey = process.env.REACT_APP_MAPS_API
+    const [position, setPosition] = useState({ lat: 43.238949, lng: 76.889709 })
+
+    const handleMapClick = (event) => {
+        console.log(event.latLng)
+        setPosition(event.latLng)
+    }
 
     const containerStyle = {
         width: '48vw',
@@ -29,6 +37,7 @@ export const Map = ({ position }) => {
                     clickableIcons={false}
                     draggable={true}
                     options={defaultMapOptions}
+                    onClick={handleMapClick}
                 >
 
 
